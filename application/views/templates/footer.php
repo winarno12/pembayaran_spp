50dff5ced07f4d5b9f665f3170b0c5123291e2ff
winarno12 <narnowin0704@gmail.com>
2022-01-19 09:53:18 +0700

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,9 +1,16 @@
 </body>
-<script src="<?= base_url('assets/js/jquery-1.10.2.js'); ?>"></script>
+<script src="<?= base_url('assets/js/jquery.min.js'); ?>"></script>
 <!-- BOOTSTRAP SCRIPTS  -->
 <script src="<?= base_url('assets/js/bootstrap.min.js'); ?> "></script>
 <!-- CUSTOM SCRIPTS  -->
-<script src="<?= base_url('assets/js/custom.js'); ?> "></script>
-<!--  -->
+<!-- <script src="<?= base_url('assets/js/custom.js'); ?> "></script> -->
+
+<script src="<?= base_url('assets/datatables/js/jquery.dataTables.min.js'); ?>"></script>
+<script src="<?= base_url('assets/datatables/js/dataTables.bootstrap5.js'); ?>"></script>
+<script>
+    $(document).ready(function() {
+        $('#myTable').DataTable();
+    });
+</script>
 
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pembayaran SPP</title>
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?= base_url('assets/datatables/css/dataTables.bootstrap5.css'); ?>">
+     
 </head>
 
 <body>
