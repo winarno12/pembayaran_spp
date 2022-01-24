b277e619858d89dfd0bd93951ea3386c6d55b217
winarno12 <narnowin0704@gmail.com>
2022-01-24 15:14:34 +0700

    Update sistem_helper.php

--- application/helpers/sistem_helper.php
+++ application/helpers/sistem_helper.php
@@ -35,3 +35,16 @@ if (!function_exists('uangtodb')) {
 		}
 	}
 }
+if (!function_exists('konversi_uang')) {
+	function konversi_uang($a)
+	{
+		if (isset($a)) {
+			if ($a == '') {
+				$a = 0;
+			}
+			$p 			= strlen($a);
+			$hasil 		= number_format($a, 2);
+			return "Rp. " . $hasil;
+		}
+	}
+}
