6868ae1373ac38aaafd29a79c160993d6bb18bf8
winarno12 <narnowin0704@gmail.com>
2022-01-25 11:50:40 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -9,18 +9,14 @@
     <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/fontawesome/css/all.css'); ?>">
     <link rel="stylesheet" href="<?= base_url('assets/datatables/css/dataTables.bootstrap5.css'); ?>">
-    <style>
-        li a:hover {
-            background-color: #111;
-        }
-    </style>
+
 </head>
 
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="<?= base_url('assets/img/fetchimage.jpg'); ?>" alt="pembayaran spp" style="width:20px;" class="rounded-pill">
+                <img src="<?= base_url('assets/img/fetchimage.jpg'); ?>" alt="Avatar Logo" style="width:20px;" class="rounded-pill"> <Span>pembayan SPP</Span>
             </a>
             <div class="float-end">
                 <ul class="navbar-nav me-auto">
@@ -37,6 +33,12 @@
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="<?= base_url('pembayaran'); ?>">Pembayaran</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Profile</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="<?= base_url('petugas/logout'); ?>">Logout</a></li>
+                        </ul>
                     </li>
                 </ul>
             </div>
