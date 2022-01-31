7bf1eb817182b806924e45484c63c9fbaefb48ca
winarno12 <narnowin0704@gmail.com>
2022-01-31 09:44:35 +0700

    update

--- application/controllers/laporan.php
+++ application/controllers/laporan.php
@@ -1,9 +1,13 @@
 <?php
 class laporan extends CI_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('fpdf');
+    }
     public  function index()
     {
-        $data['content'] = 'laporan/daftar';
-        $this->load->view('templates/main_view', $data);
+        $this->load->view('laporan/daftar');
     }
 }
