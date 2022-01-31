24c6aa0bac6f7eb2bf7089b42de85d4fb61fa620
winarno12 <narnowin0704@gmail.com>
2022-01-31 10:50:33 +0700

    updates

--- application/controllers/laporan.php
+++ application/controllers/laporan.php
@@ -4,9 +4,15 @@ class laporan extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('laporanModel');
+        $this->load->helper('sistem_helper');
     }
     public  function index()
     {
-        $this->load->view('laporan/index');
+        $data = [
+            'pembayaran'    => $this->laporanModel->getlaporan(),
+            'content'       => 'laporan/index'
+        ];
+        $this->load->view('templates/main_view', $data);
     }
 }
--- application/views/laporan/index.php
+++ application/views/laporan/index.php
@@ -1,42 +1,94 @@
-<!DOCTYPE html>
-<html lang="en">
+<style>
+    th {
+        font-size: 14px !important;
+        font-weight: bold !important;
+        text-align: center !important;
+        margin: 0 auto;
+        vertical-align: middle !important;
+    }
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pembayaran SPP</title>
-    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    td {
+        font-size: 12px !important;
+        font-weight: normal !important;
+        text-align: center !important;
+    }
 
-</head>
+    select {
+        display: inline-block;
+        padding: 4px 6px;
+        margin-bottom: 0px !important;
+        font-size: 14px;
+        line-height: 20px;
+        color: #555555;
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
+    }
 
-<body>
+    label {
+        display: inline !important;
+        width: 50% !important;
+        margin: 0 !important;
+        padding: 0 !important;
+        vertical-align: middle !important;
+    }
+</style>
+<div class="row">
     <div class="container">
-        <div class="row">
-            <div class="col">
-                <h3>Riwayat Pembayaran</h3>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                    </tbody>
-                </table>
+        <div class="col-md-12 col-lg-12 col-xl-12 mt-4 px-4">
+            <h6 class="page-title h5">
+                Daftar Siswa <small class="text-success h7">Kelola Siswa</small>
+            </h6>
+            <?= $this->session->flashdata('pesan');
+            ?>
+
+            <div class="card bg-transparent border-info ">
+                <div class="card-header bg-info">
+                    <div class="row">
+                        <div class="col-md-11">
+                            Daftar
+                        </div>
+                        <div class="col-md-1 float-right">
+                            <a href="<?= base_url('siswa/tambah_siswa'); ?>" class="btn btn-outline-primary">Cetak</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-striped table-bordered table-hover table-full-width" id="myTable">
+                            <thead>
+                                <tr>
+                                    <th>NO</th>
+                                    <th>Nama Siswa</th>
+                                    <th>Kelas</th>
+                                    <th>Tanggal / Bulan</th>
+                                    <th>Total Bayar</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php if ($pembayaran == null) : ?>
+                                    <tr>
+                                        <td colspan="5">Siswa</td>
+                                    </tr>
+                                <?php else : ?>
+                                    <?php $no = 1;
+                                    foreach ($pembayaran as $val) :
+
+                                    ?>
+                                        <tr>
+                                            <td><?= $no++ ?></td>
+                                            <td><?= $val['nama']; ?></td>
+                                            <td><?= $val['nama_kelas']; ?></td>
+                                            <td><?= $val['bulan_dibayar']; ?>/<?= $val['tahun_dibayar']; ?></td>
+                                            <td><?= konversi_uang($val['jumlah_bayar']); ?></td>
+                                        </tr>
+                                    <?php endforeach; ?>
+                                <?php endif; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-
-</body>
-
-</html>
+</div>
